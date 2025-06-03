4d8e35bf239c50f8ca73af7cf87476ca397beec6
johan231195 <johanestebensmatostuncar@gmail.com>
2025-06-03 12:21:43 -0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Bienvenido a Joykastore 👟</h1>
+      <p>Tu tienda de zapatillas favorita ya está en línea.</p>
+    </div>
+  )
+}
